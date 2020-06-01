c278a761ecbd8c915412ee7640aebda6925b540a
Suneelvarma5fi <suneelvarma55@gmail.com>
2020-06-01 14:52:39 +0530

    pymysql

--- django_projects/wsgi.py
+++ django_projects/wsgi.py
@@ -8,8 +8,9 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+import pymysql
+pymysql.install_as_MySQLdb()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_projects.settings')
 
