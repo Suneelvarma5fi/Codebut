9c5753d6dd85872e093ca7ddbe40ebd39ff400f3
Suneelvarma5fi <suneelvarma55@gmail.com>
2020-06-01 15:31:33 +0530

    Env Var Update

--- django_env/myenvdata.py
+++ django_env/myenvdata.py
@@ -1,3 +1,5 @@
 import os
 os.environ['MY_EMAIL'] = 'suneelvarma55@gmail.com'
 os.environ['MY_PASS'] = 'Suneelvarma@1512103277'
+os.environ['SECRET_KEY'] = '3b389143e8f53ec2ef088ceba60b5db8cf753b57fb4e4c40cc05509c8b432016'
+os.environ['DEBUG_VALUE'] = "True"
--- django_projects/settings.py
+++ django_projects/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'nan2bb_gss79hqp5)hq!rv2vos2z@$)ge9kgybjx@bmy-&sjaj'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['notblah.herokuapp.com']
 
 
 # Application definition
